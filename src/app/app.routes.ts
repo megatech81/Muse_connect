be4cca125112e9952ea0255192c0f1985510e512
teja13122 <39758488+megatech81@users.noreply.github.com>
2024-05-14 12:46:23 -0500

    updated signin and signup links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,21 +5,28 @@ import { UserListComponent } from './user-list/user-list.component';
 import { ChatWindowComponent } from './chat-window/chat-window.component';
 
 export const routes: Routes = [
-	{
-		path: '', redirectTo:'signup' , pathMatch:'full'
-	},
-	{
-		path:'signup' ,
-		component:SignupComponent
-	},
-	{
-		path: '',
-		component:UserListComponent,
-		children:[
-			{
-				path:'chatwindow',
-				component:ChatWindowComponent
-			}
-		]
-	}
+    {
+        path: '',
+        redirectTo: 'signup',
+        pathMatch: 'full'
+    },
+    {
+        path: 'signup',
+        component: SignupComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'users',
+        component: UserListComponent,
+        children: [
+            {
+                path: 'chat',
+                component: ChatWindowComponent
+            }
+        ]
+    }
 ];
+
